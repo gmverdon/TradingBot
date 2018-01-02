f3f96b2f30ae5f488b380f9601f9426585d59387
gmverdon <gmverdon@users.noreply.github.com>
2018-01-02 19:48:32 +0100

    Add header with html rig

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+class Header extends Component {
   render() {
     return (
-      <h1>Current price: {this.state.currentPrice}</h1>
+      <div>
+        <div>
+          <h2>TradingMaster</h2>
+        </div>
+        <div>
+          <h5>Link4</h5>
+          <h5>Link3</h5>
+          <h5>Link2</h5>
+          <h5>Link1</h5>
+        </div>
+      </div>
     )
   }
 }
