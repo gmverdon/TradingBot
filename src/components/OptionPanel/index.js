ec6a6b94a9f94767ed670936f7458028b2fc2c7c
gmverdon <gmverdon@users.noreply.github.com>
2018-01-23 19:37:04 +0100

    Add optionPanel component

---
+++ src/components/OptionPanel/index.js
@@ -0,0 +1,54 @@
+import React, {PureComponent} from 'react';
+import './styles.css';
+
+// Libraries
+import { Card, CardBody, CardTitle, CardText, Button, ButtonGroup } from 'reactstrap';
+
+const options = [
+  {
+    label: "Enable",
+    value: true,
+    color: "success"
+  },
+  {
+    label: "Disable",
+    value: false,
+    color: "danger"
+  }
+]
+
+class OptionPanel extends PureComponent {
+
+  getButtons = () => {
+    const buttonList = [];
+    for (var i = 0; i < options.length; i++) {
+      buttonList.push(this.generateButton(options[i].label, options[i].value, options[i].color, i));
+    }
+    return buttonList;
+  }
+
+  generateButton(label, value, color, counter) {
+    return <Button key={counter} color={color} onClick={() => this.handleClick(value)}>{label}</Button>
+  }
+
+  handleClick = (value) => {
+    this.props.onChange(value);
+  }
+
+  render() {
+    const {title, description} = this.props;
+    return (
+      <Card>
+        <CardBody>
+          <CardTitle>{title}</CardTitle>
+          <CardText>{description}</CardText>
+          <ButtonGroup>
+            {this.getButtons()}
+          </ButtonGroup>
+        </CardBody>
+      </Card>
+    )
+  }
+};
+
+export default OptionPanel;
